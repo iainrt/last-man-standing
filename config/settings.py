a37db59aaca6b942a59659c7cfffe93a0dc4a53e
iainrt <iainrt@gmail.com>
2026-05-25 22:56:18 +0100

    Sprint 1 foundation complete

--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
         "PORT": config("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
