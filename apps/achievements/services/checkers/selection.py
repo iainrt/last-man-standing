42aaf4e8f5717d3d2cfa53092c30f6ee99e8a4db
iainrt <iainrt@gmail.com>
2026-07-06 20:12:12 +0100

    Unlock first pick and joker achievements

--- apps/achievements/services/checkers/selection.py
+++ apps/achievements/services/checkers/selection.py
@@ -1,4 +1,4 @@
-from apps.achievements.services.unlock_service import unlock_achievement, update_achievement_progress
+from apps.achievements.services.unlock_service import unlock_achievement
 
 
 def check_selection_achievements(selection):
@@ -6,13 +6,21 @@ def check_selection_achievements(selection):
     Called whenever a player makes or updates a selection.
     """
 
-    unlock_achievement(
-        selection.competition_member.user,
-        "first_pick",
+    results = []
+
+    results.append(
+        unlock_achievement(
+            selection.competition_member.user,
+            "first_pick",
+        )
     )
 
     if selection.is_joker:
-        unlock_achievement(
-            selection.competition_member.user,
-            "joker_played",
+        results.append(
+            unlock_achievement(
+                selection.competition_member.user,
+                "joker_played",
+            )
         )
+
+    return results
